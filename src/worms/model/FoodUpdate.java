ddaf838e0cdb07d45a125b62f2b20530f7b3d30a
Pyjy <shadow96321@hotmail.com>
2014-04-11 13:31:01 +0200

    Update FoodUpdate.java

--- src/worms/model/FoodUpdate.java
+++ src/worms/model/FoodUpdate.java
@@ -13,7 +13,7 @@ public class Food {
 	 * Declaration of variables.
 	 */
 	private double x, y;
-	private final double radius = 0.2;
+	public static final double radius = 0.2;
 	private final double radiusIncrease = 1.1;
 
 	/**
@@ -28,7 +28,7 @@ public class Food {
 	 * @post	The provided y-coordinate has to be equal to the y-coordinate of this piece of food.
 	 * 			| (new this).getY() == y
 	 */
-	public Food(double x, double y){
+	public Food(double x, double y) {
 		this.x = x;
 		this.y = y;
 	}
@@ -39,20 +39,20 @@ public class Food {
 	 * @return this.x
 	 * 			The x-coordinate of this piece of food
 	 */
-	public double getX(){
+	public double getX() {
 		return this.x;
 	}
-	
+
 	/**
 	 * Function that returns the y-coordinate of this piece of food.
 	 * 
 	 * @return this.y
 	 * 			The y-coordinate of this piece of food
 	 */
-	public double getY(){
+	public double getY() {
 		return this.y;
 	}
-	
+
 	/**
 	 * Function that returns how much the worm, that ate this piece of food, its radius has to be 
 	 * increased.
@@ -60,18 +60,18 @@ public class Food {
 	 * @return this.radiusIncrease
 	 * 			The amount the worm's radius has to be increased.
 	 */
-	public double getRadiusIncrease(){
+	public double getRadiusIncrease() {
 		return this.radiusIncrease;
 	}
-	
+
 	/**
 	 * Function that returns the radius of this piece of food.
 	 * 
 	 * @return this.radius
 	 * 			The radius of this piece of food
 	 */
-	public double getRadius(){
-		return this.radius;
+	public double getRadius() {
+		return radius;
 	}
 
 	/**
