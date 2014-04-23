92519bc3036a2a2dddf44d1f96e3a42a57ac0444
Pyjy <shadow96321@hotmail.com>
2014-04-23 02:48:46 +0200

    Update World.java

--- src/worms/model/World.java
+++ src/worms/model/World.java
@@ -28,7 +28,7 @@ public class World {
 	private ArrayList<Food> collectionOfFood = new ArrayList<Food>();
 	private ArrayList<Team> collectionOfTeams = new ArrayList<Team>();
 	private Worm currentWorm;
-	
+
 	/**
 	 * Constructor of the class World.
 	 * 
@@ -37,27 +37,31 @@ public class World {
 	 * @param height
 	 * 			The height of the world
 	 * @param passableMap
-	 * 			... 
+	 * 			An array containing the impassable/passable value in all the pixels of the image file that is the world 
 	 * @param random
-	 * 			...
+	 * 			A random generator which is used as a seed to randomize aspects of the game world
 	 * @post	The width of the world has to be equal to the provided width.
 	 * 			| (new this).getWidth() == width
 	 * @post	The height of the world has to be equal to the provided height.
 	 * 			| (new this).getHeight() == height
+	 * @post	The calculated game map (impassable/passable per pixel) has to be equal to the array area.
+	 * 			| (new this).area = passableMap
+	 * @post	The random seed generator has to be equal to the random seed generator provided by the arguments.
+	 * 			| (new this).randomSeed = random
 	 * @throws	IllegalArgumentException
 	 * 			The provided width and/or the provided height are invalid.
 	 * 			|!this.canHaveAsWidth(this.getWidth()) || !this.canHaveAsHeight(this.getHeight())
 	 */
 	public World(double width, double height, boolean[][] passableMap,
 			Random random) throws IllegalArgumentException {
-		if (!isValidWidth(this.getWidth()) || !isValidHeight(this.getHeight()))
+		if (!isValidWidth(width) || !isValidHeight(height))
 			throw new IllegalArgumentException();
 		this.width = width;
 		this.height = height;
 		this.area = passableMap;
 		this.randomSeed = random;
 	}
-	
+
 	/**
 	 * Function that returns the width of this world.
 	 * 
@@ -77,11 +81,17 @@ public class World {
 	public double getHeight() {
 		return this.height;
 	}
-	
+
+	/**
+	 * Function that returns the random seed generator to randomize certain properties of this world.
+	 * 
+	 * @return this.randomSeed
+	 * 			The random seed generator for this world
+	 */
 	public Random getRandomSeed() {
 		return this.randomSeed;
 	}
-	
+
 	/**
 	 * Function that returns the current worm of this world (i.e. the worm whose turn it is).
 	 * 
@@ -103,10 +113,13 @@ public class World {
 	}
 
 	/**
-	 * Function that returns the name of the winner.
+	 * Function that returns the name of the winner if that worm is playing solo.
+	 * This function will return the team's name of the worm(s) that has (have) won.
+	 * If there is no winner yet, this function will null.
 	 * 
 	 * @return nameOfWinner
-	 * 			The name of the winner of the game
+	 * 			The name of the winner of the game, if the last worm that is alive, is playing solo.
+	 * 			The name of the winner's team, if the only worms alive, are those of the same team.
 	 * @return null
 	 * 			The game isn't finished yet: there is no winner
 	 */
@@ -133,7 +146,7 @@ public class World {
 	public Collection<Team> getTeams() {
 		return this.collectionOfTeams;
 	}
-	
+
 	/**
 	 * Function that returns all the worms that are currently in this world (i.e. in the game).
 	 * 
@@ -144,26 +157,43 @@ public class World {
 		return this.collectionOfWorms;
 	}
 
+	/**
+	 * Function that returns all objects (i.e. worms, food, teams, active projectile) in this world.
+	 * 
+	 * @return objectsList
+	 * 			An ArrayList containing all the objects in this world
+	 */
 	public ArrayList<Object> getObjectsInWorld() {
 		ArrayList<Object> objectsList = new ArrayList<Object>();
 		objectsList.addAll(this.collectionOfWorms);
 		objectsList.addAll(this.collectionOfTeams);
 		objectsList.addAll(this.collectionOfFood);
+		objectsList.add(this.getActiveProjectile());
 		return objectsList;
 	}
-	
+
 	/**
 	 * Function that returns the active projectile from this world.
 	 * 
-	 * @return this.activeProjectile
+	 * @return this.getCurrentWorm().getProjectile()
 	 * 			The projectile that is active in this world
 	 */
 	public Projectile getActiveProjectile() {
 		return this.getCurrentWorm().getProjectile();
 	}
-	
+
 	/**
-	 * Function that adds a worm to this world.
+	 * Function that adds a worm to this world on an arbitrarily chosen location, adjacent to impassable terrain.
+	 * If the game has already been started, it is no longer allowed to add any worms to this world.
+	 * The values of the properties of the newly created worm have to be valid when the new worm is being created.
+	 * The worm is added to the collection of worms of this world and is arbitrarily assigned to an arbitrarily 
+	 * chosen team, if there already are teams available in this world.
+	 * 
+	 * @post	The newly created worm has been added to this world.
+	 * 			| (new this).getWorms().size == this.getWorms().size + 1 
+	 * 			| 	&& (new this).getWorms().get((new this).getWorms().size() - 1) == newWorm
+	 * @post	The newly created worm CAN have a team.
+	 * 			| (new this).getWorms().get((new this).getWorms().size() - 1).getTeamName() == teamName
 	 */
 	public void addWormToWorld() {
 		if (this.isStarted())
@@ -189,7 +219,16 @@ public class World {
 								this.collectionOfTeams.get(randomGen
 										.nextInt(this.collectionOfTeams.size())));
 	}
-	
+
+	/**
+	 * Method that removes a given worm from this world.
+	 * 
+	 * @param worm
+	 * 			The worm that has to be removed from this world
+	 * @post	The worm that is provided has to be deleted from this world.
+	 * 			| (new this).getWorms().size() == this.getWorms().size() - 1 
+	 * 			|	&& !(new this).getWorms().contains(worm) && worm.isTerminated()
+	 */
 	public void removeWormFromWorld(Worm worm) {
 		worm.terminate();
 		this.collectionOfWorms.remove(worm);
@@ -197,6 +236,12 @@ public class World {
 
 	/**
 	 * Function that adds a piece of food to this world.
+	 * The position of the piece of food that is to be added is chosen arbitrarily, but will always be adjacent to impassable terrain.
+	 * Once the game has been started, it is no longer allowed to add any more additional pieces of food to this world.
+	 * 
+	 * @post	The newly created piece of food has been added to this world.
+	 * 			| (new this).getFood().size == this.getFood().size + 1 
+	 * 			| 	&& (new this).getFood().get((new this).getFood().size() - 1) == newFood
 	 */
 	public void addFoodToWorld() {
 		if (this.isStarted())
@@ -211,42 +256,63 @@ public class World {
 		} while (this.collectionOfFood.size() != oldNumberOfFood + 1);
 	}
 
+	/**
+	 * Method that removes a given piece of food from this world.
+	 * 
+	 * @param food
+	 * 			The piece of food that has to be removed from this world.
+	 * @post	The piece of food that is provided has to be deleted from this world.
+	 * 			| (new this).getFood().size() == this.getFood().size() - 1 
+	 * 			|	&& !(new this).getFood().contains(food) && food.isTerminated()
+	 */
 	public void removeFoodFromWorld(Food food) {
 		food.terminate();
 		this.collectionOfFood.remove(food);
 	}
-	
+
 	/**
-	 * Function that adds a new team of worms to this world.
+	 * Function that adds a new team of worms to this world, when it is possible (no more than 10 teams can be added to this world).
 	 * 
 	 * @param newName
 	 * 			The name of the new team
+	 * @post	The new team has been added to this world.
+	 * 			| (new this).getTeams().size == this.getTeams().size() + 1 
+	 * 			|	&& (new this).getTeams().size() <= 10 
+	 * 			|		&& (new this).getTeams().get((new this).getTeams().size() - 1) == newTeam
+	 * 			|			&& (new this).getTeams().get((new this).getTeams().size() - 1).equals(newName)
 	 */
 	public void addEmptyTeam(String newName) {
 		if (this.canCreateTeam())
 			this.collectionOfTeams.add(new Team(newName));
 	}
 	
+	public void removeProjectileFromWorld(Projectile projectile) {
+		projectile.terminate();
+	}
+
 	/**
 	 * Method that creates a new piece of food, adds it to the food of this world (i.e. the collection) 
 	 * and returns the newly created piece of food.
+	 * Once the game has beens started, no more food is allowed to be added to this world.
 	 * 
 	 * @param x
 	 * 			The x-coordinate of the food that has to be created
 	 * @param y
 	 * 			The y-coordinate of the food that has to be created
-	 * @return food
+	 * @return newFood
 	 * 			The newly created food
 	 */
-	public Food createFood(double x, double y) {
-		Food food = new Food(this, x, y);
-		this.collectionOfFood.add(food);
-		return food;
+	public Food createFood(double x, double y) throws RuntimeException {
+		if (this.isStarted())
+			throw new RuntimeException();
+		this.collectionOfFood.add(new Food(this, x, y));
+		return this.collectionOfFood.get(this.collectionOfFood.size() - 1);
 	}
 
 	/**
 	 * Method that creates a new worm, adds it to the collection of worms that are part of this world 
 	 * and then returns the newly created worm.
+	 * Once the game has been started, it is no longer allowed to add new worms to this world.
 	 * 
 	 * @param x
 	 * 			The x-coordinate of the to be created worm
@@ -258,33 +324,48 @@ public class World {
 	 * 			The radius of the to be created worm
 	 * @param name
 	 * 			The name of the to be created worm
-	 * @return worm
+	 * @post	The new worm has been added to this world.
+	 * 			| (new this).getWorms().size() == this.getWorms().size() + 1
+	 * 			|	&& (new this).getWorms().get((new this).getWorms().size() - 1) == newWorm
+	 * @return newWorm
 	 * 			The newly created worm
 	 */
 	public Worm createWorm(double x, double y, double direction, double radius,
-			String name) {
+			String name) throws RuntimeException {
+		if (this.isStarted())
+			throw new RuntimeException();
 		Worm worm = new Worm(this, x, y, direction, radius, name);
 		this.collectionOfWorms.add(worm);
 		return worm;
 	}
-	
+
 	/**
-	 * Function that starts the game.
+	 * Method that starts the game.
+	 * This method sets the state of the game in this world to started and it also selects the 
+	 * first worm that will be allowed to do player-controlled actions.
+	 * If there are currently no worms in this world, the game will end.
+	 * 
+	 * @post	This method sets the state of the game in this world to started.
+	 * 			| (new this).isStarted()
+	 * @post	The first worm has been selected.
+	 * 			| (new this).getWorms().get(0) == (new this).getCurrentWorm()
 	 */
-	public void startGame() throws RuntimeException {
-		if (this.collectionOfWorms.isEmpty())
-			// return;
-			throw new RuntimeException();
-		this.currentWorm = this.collectionOfWorms.get(0);
+	public void startGame() {
 		this.isStarted = true;
+		if (this.collectionOfWorms.isEmpty())
+			return;
+		this.currentWorm = this.collectionOfWorms.get(0);
 	}
-	
+
 	/**
 	 * Function that starts a next turn.
 	 * Each new turn, the worm whose turn it is, is healed for 10 Hit Points and its 
 	 * Action Points are reset to the maximum amount of Action Points that worm can have.
+	 * The next worm's turn cannot start, until the active projectile of this world has been terminated.
 	 */
 	public void startNextTurn() {
+		if (!this.getActiveProjectile().isTerminated())
+			this.removeProjectileFromWorld(this.getActiveProjectile());
 		this.currentWorm = ((ArrayList<Worm>) this.getWorms())
 				.get((((ArrayList<Worm>) this.getWorms()).indexOf(this
 						.getCurrentWorm()) + 1) % getWorms().size());
@@ -294,6 +375,23 @@ public class World {
 				this.getCurrentWorm().getMaxActionPoints());
 	}
 
+	/**
+	 * Method that checks the location status of this world for an object that stands on the given coordinates (x,y) and has the given radius.
+	 * 
+	 * @param x
+	 * 			The x-coordinate of the object
+	 * @param y
+	 * 			The y-coordinate of the object
+	 * @param radius
+	 * 			The radius of the worm
+	 * @effect	The boolean values impassable, adjacent and passable are set to the correct values for the object's position and radius.
+	 * 			| if (object.isImpassable(object.getX(), object.getY, object.getRadius())
+	 * 			|	then ((new this).impassable && !((new this).adjacent) && !((new this).passable))
+	 * 			| else if (object.isAdjacent(object.getX(), object.getY, object.getRadius())
+	 * 			|	then (!((new this).impassable) && (new this).adjacent && !((new this).passable))
+	 * 			| else if (object.isPassable(object.getX(), object.getY, object.getRadius())
+	 * 			|	then (!((new this).impassable) && !((new this).adjacent) && (new this).passable)
+	 */
 	public void calculateLocationStatus(double x, double y, double radius) {
 		double circleX, circleY;
 		this.impassable = false;
@@ -323,7 +421,7 @@ public class World {
 	 * Function that checks whether or not the object with provided (x,y)-coordinates and radius is adjacent to
 	 * impassable terrain or not.
 	 * An object is adjacent to impassable terrain when the circle describing the object is on impassable terrain 
-	 * with 1.1 times the radius of the object.
+	 * with 1.1 times the radius of the object and on passable terrain with the radius of the object.
 	 *
 	 * @param x
 	 * 			The x-coordinate of the object (worm, food etc.)
@@ -361,6 +459,21 @@ public class World {
 		return this.impassable;
 	}
 
+	/**
+	 * Function that checks whether or not all terrain on the circle, described by the 
+	 * provided coordinates and the provided radius is all passable.
+	 * 
+	 * @param x
+	 * 			The x-coordinate of the middle point
+	 * @param y
+	 * 			The y-coordinate of the middle point
+	 * @param radius
+	 * 			The radius of the circle
+	 * @return true
+	 * 			All terrain is passable on the provided radius around the provided coordinates
+	 * @return false
+	 * 			Not all of the terrain is passable on the provided radius around the provided coordinates
+	 */
 	public boolean isPassable(double x, double y, double radius) {
 		this.calculateLocationStatus(x, y, radius);
 		return this.passable;
@@ -420,7 +533,7 @@ public class World {
 		return ((x + radius <= this.getWidth()) && (x - radius >= lowerBoundX)
 				&& (y + radius <= this.getHeight()) && (y - radius >= lowerBoundY));
 	}
-	
+
 	/**
 	 * Function that checks whether or not the provided width is valid.
 	 * 
@@ -450,7 +563,7 @@ public class World {
 				return true;
 		return false;
 	}
-	
+
 	private double[] locateNewObject(double radius) {
 		Random randomGen = new Random();
 		double initX = 0, initY = 0;
@@ -542,7 +655,7 @@ public class World {
 		double[] result = { -1, -1, -1 };
 		return result;
 	}
-	
+
 	private int[] metricToPixels(double x, double y) {
 		double heightPerPixel = this.getHeight() / this.area.length;
 		double widthPerPixel = this.getWidth() / this.area[0].length;
